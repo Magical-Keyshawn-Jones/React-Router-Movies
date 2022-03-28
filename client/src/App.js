d54aed51dbaceb7c2b05e3cd3f954b206898bce8
Keyshawn Jones <keyshawnjones29@gmail.com>
2022-03-28 18:03:38 -0500

    finished everything

--- client/src/App.js
+++ client/src/App.js
@@ -35,6 +35,9 @@ export default function App () {
     <div>
       <SavedList list={[ /* This is stretch */]} />
       <Switch>
+      <Route path={`/movies/:id`}>
+          <Movie/>
+        </Route>
         <Route path='/'>
           <MovieList movies={movieList}/>
         </Route>
